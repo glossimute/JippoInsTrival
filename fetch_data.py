c20b6fa2292fc9687ab140cadc8d719350e6fcbc
glossimute <lifepattern_@outlook.com>
2024-09-08 16:48:32 +0800

    Update fetch_data.py

--- fetch_data.py
+++ fetch_data.py
@@ -18,10 +18,9 @@ response = requests.get(url, params=params)
 if response.status_code == 200:
     data = response.json()
     print(data)
-    # 将数据写入 JSON 文件
-    with open('output_data.json', 'w', encoding='utf-8') as f:
-        json.dump(data, f, ensure_ascii=False, indent=4)
-
-    print("数据成功写入 output_data.json 文件")
+    data1 = json.loads(data)
+    with open('output.txt', 'w') as file:
+    file.write(data1["username"]+","+data1[followers_count]"\n")
+    print("数据成功写入 output.txt 文件")
 else:
     print(f"请求失败，状态码: {response.status_code}")
