69ea98290d9905a83fc021961eab07e856e14c76
glossimute <lifepattern_@outlook.com>
2024-09-08 16:59:22 +0800

    Update fetch_data.py

--- fetch_data.py
+++ fetch_data.py
@@ -5,7 +5,7 @@ import json
 # 从环境变量中获取 Access Token
 access_token = os.getenv('FB_ACCESS_TOKEN')
 # 设置 API 端点和 Access Token
-url = 'https://graph.facebook.com/v20.0/17841407991309868'
+url = os.getenu('URL')
 params = {
     'fields': 'business_discovery.username(yezyizhere){followers_count,username}',
     'access_token': access_token
@@ -18,7 +18,7 @@ response = requests.get(url, params=params)
 if response.status_code == 200:
     data = response.json()
     with open('output.txt', 'w') as file:
-    file.write(data["username"]+","+data["followers_count"]"\n")
+        file.write(data["username"]+","+data["followers_count"]"\n")
     print("数据成功写入 output.txt 文件")
 else:
     print(f"请求失败，状态码: {response.status_code}")
